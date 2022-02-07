3aa6dc87c92b763e4c04b60db9c5ed8ab2fcf281
altindalorcun <altindalorcun@gmail.com>
2022-02-07 20:54:17 +0300

    Solve Apple and Orange problem

---
+++ src/tr/hacker/rank/appleandorange/Main.java
@@ -0,0 +1,23 @@
+package tr.hacker.rank.appleandorange;
+
+import java.util.List;
+
+public class Main {
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+
+        System.out.println((int) apples.stream().map(apple -> {
+            return a + apple;
+        }).filter(appleLocation -> s <= appleLocation && t >= appleLocation).count());
+
+        System.out.println((int) oranges.stream().map(orange -> {
+            return b + orange;
+        }).filter(orangeLocation -> s <= orangeLocation && t >= orangeLocation).count());
+    }
+
+    public static void main(String[] args) {
+
+        countApplesAndOranges(7, 11, 5, 15, List.of(-2, 2, 1), List.of(5, -6));
+
+    }
+}
